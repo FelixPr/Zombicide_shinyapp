6db69820ba83c2aee509f8f92514a1c0b8585f10
FelixPr <fpryfer@gmail.com>
2016-12-30 20:32:46 +0100

    add reroll function

--- Zombicide_app_fr/server.R
+++ Zombicide_app_fr/server.R
@@ -1,63 +1,87 @@
+###############################################################
+# Load libraries
+###############################################################
 library(shiny)
 library(ggplot2)
 library(scales)
 
-# Calculate the odds. Takes as input :
-#  n: number of dices you roll
-#  p: precision of the weapon, i.e. the score you need
-#  z: number of zombies killed
+###############################################################
+# Tool box
+###############################################################
+# 1/ Calculate the probability of killing z zombies
+#   assuming you roll n dices and need a p score
 proba <- function(n, p, z) {
+  
   # The formula is given by Bernoulli 
   choose(n, z)*(p-1)^(n-z)*(7-p)^z/6^n
 }
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+###############################################################
+# 2/ Calculate probability array for an attack
+#     no reroll authorized
+odds_no_reroll <- function(n, p, zmx){
   
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-
+  # initialize the array
+  attack <- seq(0, 0, length=zmx+1)
+  
+  # calculate the odds
+  for (i in 1:(n+1)) {
+    if (i<=zmx) {  # if i<=zmx, no problem, I can calculate the odds
+      attack[i] <- proba(n, p, i-1)  # probability to kill (i-1) zombies
+    }
+    else {  # if i>zmx, I can't kill i zombies and I'll kill zmx zombies instead
+      attack[zmx+1] <- attack[zmx+1] + proba(n, p, i-1)
+    }
+  }
+  
+  # output the array
+  attack
+}  
+###############################################################
+# 3/ Calculate probability array for an attack
+#      reroll taken into account
+calculate_odds <- function(n, p, zmx, r) {
+  
+  # calculate probability without reroll
+  attack <- odds_no_reroll(n, p, zmx)
+  
+  # if needed, calculate probability with reroll
+  if (r) {
+    # calculate threshold, i.e. the max score bellow average
+    threshold <- trunc(sum(attack*(0:zmx)))
+    
+    # calculate bad luck probability, i.e. probability to get a score bellow the average 
+    BL <- sum(attack[1:(threshold+1)])  # sum the odds to kill from 0 to 'threshold' zombies
+    
+    # calculate new odds
+    attack[1:(threshold+1)] <- attack[1:(threshold+1)]*BL
+    attack[(threshold+2):length(attack)] <- attack[(threshold+2):length(attack)]*(1 + BL)
+  }
+  
+  # output the array
+  attack
+}
+###############################################################
+# ShinyServer function
+## Define server logic required to draw a histogram
+###############################################################
+shinyServer(function(input, output) {
   output$histPlot <- renderPlot({
   
     # init variables    
-    n <- input$n  # number of dices for the first weapon
-    p <- as.numeric(input$p)  # precision of the first weapon
+    n1 <- input$n  # number of dices for the first weapon
+    p1 <- as.numeric(input$p)  # precision of the first weapon
     n2 <- input$n2  # number of dices for the 2nd weapon
     p2 <- as.numeric(input$p2)  # precision of the 2nd weapon
-    nmx <- max(n, n2)  # max rolled dices
+    nmx <- max(n1, n2)  # max rolled dices
     avbZombies <- input$avbZombies  # number of zombies in the targeted area
     zmx <- min(avbZombies, nmx)  # max kills you can get
     success <- 0:zmx  # an array that store the possible numbers of success, i.e. zombies killed
+    r1 <- input$reroll1  # can you reroll attack 1?
+    r2 <- input$reroll2  # can you reroll attack 2?
     
-    # arrays that store the odds for every attack
-    attack_1 <- seq(0, 0, length=zmx+1)
-    attack_2 <- seq(0, 0, length=zmx+1)
-    
-    # calculate the odds of killing i zombies for the 1st attack
-    for (i in 1:(n+1)) {
-      if (i<=zmx) {  # if i<=zmx, no problem, I can calculate the odds
-        attack_1[i] <- proba(n, p, i-1)
-      }
-      else {  # if i>zmx, I can't kill i zombies and I'll kill zmx zombies instead
-        attack_1[zmx+1] <- attack_1[zmx+1] + proba(n, p, i-1)
-      }
-    }  
-    
-    # calculate the odds of killing i zombies for the 2nd attack
-    if (n2!=0) { # if there's a 2nd attack, I calculate
-      for (i in 1:(n2+1)) {
-        if (i<=zmx) { # if i<=zmx, no problem, I can calculate the odds
-          attack_2[i] <- proba(n2, p2, i-1)
-        }
-        else {  # if i>zmx, I can't kill i zombies and I'll kill zmx zombies instead
-          attack_2[zmx+1] <- attack_2[zmx+1] + proba(n2, p2, i-1)
-        }
-      }
-    }
+    # calculate the odds for each attack
+    attack_1 <- calculate_odds(n1, p1, zmx, r1)
+    attack_2 <- calculate_odds(n2, p2, zmx, r2)
     
     # create the dataset
     probability <- c(attack_1, attack_2)  # put the odds together
@@ -77,10 +101,10 @@ shinyServer(function(input, output) {
               axis.ticks.y=element_blank()) +
         scale_fill_discrete(name="Zombies tués\nen moyenne :",
                             breaks=c("1", "2"),
-                            labels=c(paste("attack 1 : ", round(sum((probability*zombie_kills)[dat$attack==1]), 2), sep =""),
-                                     paste("attack 2 : ", round(sum((probability*zombie_kills)[dat$attack==2]), 2), sep =""))) +
+                            labels=c(paste("attaque 1 : ", round(sum((probability*zombie_kills)[dat$attack==1]), 2), sep =""),
+                                     paste("attaque 2 : ", round(sum((probability*zombie_kills)[dat$attack==2]), 2), sep =""))) +
         xlab("Zombies tués") + 
-        ylab("probability")
+        ylab("Probabilités")
     }
     else { # now there's a 2nd attack
       d1 <- ggplot(data=dat[dat$attack==1, ], aes(x=zombie_kills, y=probability, fill=attack, label = percent(probability))) +
@@ -93,8 +117,8 @@ shinyServer(function(input, output) {
               axis.ticks.y=element_blank()) +
         scale_fill_discrete(name="Zombies tués\nen moyenne :",
                             breaks=c("1", "2"),
-                            labels=c(paste("attack 1 : ", round(sum((probability*zombie_kills)[dat$attack==1]), 2), sep =""),
-                                     paste("attack 2 : ", round(sum((probability*zombie_kills)[dat$attack==2]), 2), sep =""))) +
+                            labels=c(paste("attaque 1 : ", round(sum((probability*zombie_kills)[dat$attack==1]), 2), sep =""),
+                                     paste("attaque 2 : ", round(sum((probability*zombie_kills)[dat$attack==2]), 2), sep =""))) +
         xlab("Zombies tués") + 
         ylab("Probabilités")
     }
@@ -103,3 +127,4 @@ shinyServer(function(input, output) {
     d1
   })
 })
+      
--- Zombicide_app_fr/ui.R
+++ Zombicide_app_fr/ui.R
@@ -3,45 +3,68 @@ library(ggplot2)
 library(scales)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
+shinyUI(
+  fluidPage(
+    
   # Application title
   titlePanel("Zombicide"),
   
-  # Sidebar
-  sidebarLayout(
-    sidebarPanel(
-      helpText("1/ Sélectionne les caractéristiques de ton attaque :"),
-      numericInput("n",
-                  "Nombre de dés :",
-                  min = 1,
-                  max = 50,
-                  value = 3),
-      radioButtons("p",
-                   "Précision :",
-                   c("6+" = 6, "5+"=5, "4+"=4, "3+"=3, "2+"=2)),
-      helpText("Tu n'as pas beaucoup de zombies à portée ? 
-               Sélectionne le nombre de zombies max :"),
-      numericInput("avbZombies",
-                  "Zombies sur la case ciblée :",
-                  min = 1,
-                  max = 50,
-                  value = 36),
-      helpText("2/ Tu hésites entre deux armes ?
-               Sélectionne les caractéristiques de la deuxième arme :"),
-      numericInput("n2",
-                  "Nombre de dés :",
-                  min = 0,
-                  max = 50,
-                  value = 0),
-      radioButtons("p2",
-                   "Précision :",
-                   c("6+" = 6, "5+"=5, "4+"=4, "3+"=3, "2+"=2))
-      
+  fluidRow(
+    # Left panel
+    column(3,
+           # Attack 1
+           wellPanel(
+             h4("Attaque 1"),
+             numericInput("n",
+                          "Nombre de dés :",
+                          min = 1,
+                          max = 50,
+                          value = 3),
+             sliderInput("p",  "Précision :", 2, 6, 5, post = "+" ),
+             checkboxInput("reroll1", label = "Relance autorisée", value = FALSE)
+           ),
+           
+           # Attack 2
+           wellPanel(
+             h4("Attaque 2"),
+             numericInput("n2",
+                          "Nombre de dés :",
+                          min = 0,
+                          max = 50,
+                          value = 0),
+             sliderInput("p2",  "Précision :", 2, 6, 5, post = "+" ),
+             checkboxInput("reroll2", label = "Relance autorisée", value = FALSE)
+           ),
+           
+           # Max zombies
+           wellPanel(
+              numericInput("avbZombies",
+                          "Zombies sur la case ciblée :",
+                          min = 1,
+                          max = 50,
+                          value = 36)
+           
+           )
   ),
-    # Show the histogram
-    mainPanel(
-      plotOutput("histPlot")
-    )
+  
+  # Right panel
+  column(9,
+         
+         # Histogram
+         plotOutput("histPlot"),
+         
+         # Text and link
+         wellPanel(
+           h5("Conseils d'utilisation : "),
+           h5(" - Si tu as plus de dés à lancer que de zombies à tuer, utilise la troisième box 
+              pour entrer un nombre maximum. Tu verras que ça change légèrement les calculs."),
+           h5(" - Les probabilitées avec relance sont calculées en faisant l'hypothèse que 
+              l'on relance si et seulement si le premier jet est en dessous de la moyenne."),
+           h5(a("Tu veux savoir comment ça marche ? Clique ici !", 
+                href="https://github.com/FelixPr/Zombicide_shinyapp", target="_blank")
+              )
+           )
+         )
   )
-))
+  )
+)
